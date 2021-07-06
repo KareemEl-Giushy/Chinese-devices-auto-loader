1de13dc436d9390669c3094742292a299edac8a7
Kareem El-Giushy <kareem.64123@gmail.com>
2021-07-06 13:26:34 +0200

    Finish the LCD Count up

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from PyQt5.QtGui import QIcon, QPixmap
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt5.QtWidgets import QApplication, QLCDNumber, QMainWindow, QWidget
 from PyQt5.uic import loadUiType
 import os
 from os import path
@@ -30,26 +30,46 @@ class MainApp(QMainWindow, Main_Ui):
         self.location = path.dirname(path.realpath(__file__))
         self.binFilesPath = ""
         # Start Seting Up The Ui
-        self.setFixedSize(311, 410)
+        self.setFixedSize(311, 446)
         # self.setWindowIcon(QIcon(path.join(path.dirname(__file__), 'Icon.ico')))
         self.setupApp()
 
     def setupApp(self):
         self.status.setHtml('<p align="center"><span style=" font-size:11pt; font-weight:600; color:#ff0000;">Down</span></p>')
+        self.statusPrepare.setHtml('')
+        self.statusOperation.setHtml('')
         # self.fileNumber
         self.browseFile.clicked.connect(self.browse_file)
         self.prepareFiles.clicked.connect(self.get_fileName_ready)
         self.startOperation.clicked.connect(self.start_operation)
         self.actionCreator.triggered.connect(self.creator)
-    
+
     def start_operation(self):
         print("Operation Started")
-        self.status.setHtml('<p align="center"><span style=" font-size:11pt; font-weight:600; color:#00aa00;">Up</span></p>')
         # Disable buttons
         self.startOperation.setEnabled(False)
         self.prepareFiles.setEnabled(False)
+        # Change Word
+        self.status.setHtml('<p align="center"><span style=" font-size:11pt; font-weight:600; color:#00aa00;">Up</span></p>')
+        self.statusOperation.setHtml('<p align="center"><span style=" font-size:11pt; font-weight:600;">جاري التشغيل</span></p>')
         # Start automation
+        try:
+            self.start_testing()
+        except:
+            self.startOperation.setEnabled(True)
+            self.prepareFiles.setEnabled(True)
+            self.status.setHtml('<p align="center"><span style=" font-size:11pt; font-weight:600; color:#ff0000;">Down</span></p>')
+            self.statusOperation.setHtml('<p align="center"><span style=" font-size:11pt; font-weight:600; color:#ff0000;">فشل في العملية</span></p>')
+            return 0
+        # Enable buttons again
+        self.startOperation.setEnabled(True)
+        self.prepareFiles.setEnabled(True)
+        self.status.setHtml('<p align="center"><span style=" font-size:11pt; font-weight:600; color:#ff0000;">Down</span></p>')
+        self.statusOperation.setHtml('<p align="center"><span style=" font-size:11pt; font-weight:600; color:#00aa00;">تمت العملية بنجاح</span></p>')
+    
+    def start_testing(self):
         for n in range(1,31):
+            self.fileNumber.display(n)
             lackey.click(self.location + r"\src\browse.png")
             lackey.click(self.location + r"\src\name.png")
             lackey.type("%d.bin" % n)
@@ -66,10 +86,6 @@ class MainApp(QMainWindow, Main_Ui):
                 lackey.wait(self.location + r"\src\finish.png", 8)
                 lackey.click(self.location + r"\src\finish.png")
                 break
-        # Enable buttons again
-        self.startOperation.setEnabled(True)
-        self.prepareFiles.setEnabled(True)
-        self.status.setHtml('<p align="center"><span style=" font-size:11pt; font-weight:600; color:#ff0000;">Down</span></p>')
 
     def get_fileName_ready(self):
         if path.isdir(self.binFilesPath):
@@ -78,7 +94,10 @@ class MainApp(QMainWindow, Main_Ui):
                 for f in os.listdir(self.binFilesPath):
                     os.rename(f, path.join(path.dirname(f), Counter))
                     Counter += 1
-    
+                self.statusPrepare.setHtml('<p align="center"><span style=" font-size:11pt; font-weight:600; color:#00aa00;">تم</span></p>')
+                return 0
+        self.statusPrepare.setHtml('<p align="center"><span style=" font-size:11pt; font-weight:600; color:#ff0000;">خطأ في المكان</span></p>')
+
     def browse_file(self):
         pass
 
